97f25bfb8b600e77db3a7d18dc72b8646530202e
fantonix <fantonix.web@gmail.com>
2023-01-27 21:49:28 -0400

    Renaming src/routes.tsx to components.tsx and changing their structure

---
+++ src/components.tsx
@@ -0,0 +1,15 @@
+import MainPage from "./MainPage/index.ts";
+import About from "./About/index.ts";
+import Works from "./Works/index.ts";
+import WorkWrapper from "./Work/index.ts";
+import { NotFound } from "./Errors/index.ts";
+
+const components = {
+  MainPage,
+  About,
+  Works,
+  WorkWrapper,
+  NotFound,
+};
+
+export default components;
