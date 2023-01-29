41bb8571b279ec8db64c4631c89a44f2890bb4e3
fantonix <fantonix.web@gmail.com>
2023-01-29 18:13:55 -0400

    Updating static content prefix url in Works

--- src/Works/worksPresentation.tsx
+++ src/Works/worksPresentation.tsx
@@ -9,52 +9,52 @@ export interface worksPresentationObject {
 
 const worksPresentation: worksPresentationObject[] = [
   {
-    img: "/img/SPA.webp",
-    squaredImg: "/img/SPA-squared.webp",
+    img: "/static/media/native_m/SPA.webp",
+    squaredImg: "/static/media/native_m/SPA-squared.webp",
     alternativeText:
       "Single Page Applicaation with MERN stack, google sign in, JWT, etc.",
     workUrlId: "1",
   },
   {
-    img: "/img/NFTs.webp",
-    squaredImg: "/img/NFTs-squared.webp",
+    img: "/static/media/native_m/NFTs.webp",
+    squaredImg: "/static/media/native_m/NFTs-squared.webp",
     alternativeText: "NFTs where appear Elon Musk, Lebron James, BonsAI...",
     workUrlId: "2",
   },
   {
-    img: "/img/Games.webp",
-    squaredImg: "/img/Games-squared.webp",
+    img: "/static/media/native_m/Games.webp",
+    squaredImg: "/static/media/native_m/Games-squared.webp",
     alternativeText:
       "These are the games I've made, what are you waiting for watch them?",
     workUrlId: "3",
   },
   {
-    img: "/img/BarbaJS.webp",
-    squaredImg: "/img/BarbaJS-squared.webp",
+    img: "/static/media/native_m/BarbaJS.webp",
+    squaredImg: "/static/media/native_m/BarbaJS-squared.webp",
     alternativeText: "Beatifuld and smooth transitions.",
     workUrlId: "4",
   },
   {
-    img: "/img/Bonsai.webp",
-    squaredImg: "/img/Bonsai-squared.webp",
+    img: "/static/media/native_m/Bonsai.webp",
+    squaredImg: "/static/media/native_m/Bonsai-squared.webp",
     alternativeText: "This is my BonsAI page",
     workUrlId: "5",
   },
   {
-    img: "/img/Friends.webp",
-    squaredImg: "/img/Friends-squared.webp",
+    img: "/static/media/native_m/Friends.webp",
+    squaredImg: "/static/media/native_m/Friends-squared.webp",
     alternativeText: "They are the best choice and achieve I have made",
     workUrlId: "6",
   },
   {
-    img: "/img/Blog.webp",
-    squaredImg: "/img/Blog-squared.webp",
+    img: "/static/media/native_m/Blog.webp",
+    squaredImg: "/static/media/native_m/Blog-squared.webp",
     alternativeText: "This is my blog, I should make it interesting",
     workUrlId: "7",
   },
   {
-    img: "/img/Projects.webp",
-    squaredImg: "/img/Projects-squared.webp",
+    img: "/static/media/native_m/Projects.webp",
+    squaredImg: "/static/media/native_m/Projects-squared.webp",
     alternativeText:
       "There are projects, but projects are ideas for more projects, so let's make more projects",
     workUrlId: "8",
@@ -62,8 +62,8 @@ const worksPresentation: worksPresentationObject[] = [
 ];
 
 const NA: worksPresentationObject = {
-  img: "/img/NA.webp",
-  squaredImg: "/img/NA-squared.webp",
+  img: "/static/media/native_m/NA.webp",
+  squaredImg: "/static/media/native_m/NA-squared.webp",
   alternativeText: "It's a building's window, there's no work there already",
   workUrlId: "/",
 };
