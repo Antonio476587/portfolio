64711011bc62a50f1a641a890b0e2a039674c22b
fantonix <fantonix.web@gmail.com>
2023-01-29 19:09:39 -0400

    Creating the initializer for the firebase app and services

---
+++ utils/firebaseInitializer.ts
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
+import {  getStorage } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: Deno.env.get("FIREBASE_API_KEY"),
+  authDomain: Deno.env.get("FIREBASE_AUTH_DOMAIN"),
+  projectId: Deno.env.get("FIREBASE_PROJECT_ID"),
+  storageBucket: Deno.env.get("FIREBASE_STORAGE_BUCKET"),
+  messagingSenderId: Deno.env.get("FIREBASE_MESSAGING_SENDER_ID"),
+  appId: Deno.env.get("FIREBASE_APP_ID"),
+  measurementId: Deno.env.get("FIREBASE_MEASUREMENT_ID"),
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+const storage = getStorage(app);
+
+export {
+    app,
+    analytics,
+    storage
+}
+export * from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+export * from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
+export * from "https://www.gstatic.com/firebasejs/9.16.0/firebase-storage.js";
