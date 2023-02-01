de6bae60e6674ed77d26e5368fa42aea85172b96
fantonix <fantonix.web@gmail.com>
2023-01-31 21:20:19 -0400

    Changing the template engine language pug for ejs in all the project

--- templates/templateHome.js
+++ templates/templateHome.js
@@ -1,28 +1,27 @@
 import templateCompiler from "../utils/templateCompiler.ts";
-import gsapScripts from "../templates/head/gsapScriptsHTMLTemplate.js";
 
 export default async function template(body, rootNodeName) {
     return `<!DOCTYPE html>
     <html lang="en" style="background-color: #010101;">
-    ${await templateCompiler("templates/head/head.pug", { headTags: templateCompiler.convertToValidPugInsertion(gsapScripts )})}
+    ${await templateCompiler("templates/head/head.ejs")}
   
   <body allow="fullscreen *">
   
     <div class="body" id="body">
   
-      ${await templateCompiler("templates/elements/preload.pug")}
+      ${await templateCompiler("templates/elements/preload.ejs")}
 
       <!-- Contact page -->
       <!-- Main -->
       <div id="${rootNodeName}">${body}</div>
 
-      ${await templateCompiler("templates/elements/home.pug")}
+      ${await templateCompiler("templates/elements/home.ejs")}
 
       </div>
       
       <!-- App -->
       <script src="/dynamic/js/${rootNodeName}.js" type="application/javascript"></script>
-      ${await templateCompiler("templates/scripts/scripts.pug")}
+      ${await templateCompiler("templates/scripts/scripts.ejs")}
       <!-- Home -->
       <script src="/static/js/home.js" type="application/javascript"></script>
     </body>
--- utils/templateCompiler.ts
+++ utils/templateCompiler.ts
@@ -3,32 +3,11 @@ import minimist from "https://esm.sh/minimist@1.2.7";
 
 const argvFlags = minimist(Deno.args);
 
-import pug, { LocalsObject, Options } from "https://esm.sh/pug@3.0.2";
+import ejs, { Options } from "https://esm.sh/ejs@3.1.8";
 
 import { __dirname as __root_dirname } from "../pathEMS.js";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import htmlSyntax from "../templates/head/gsapScriptsHTMLTemplate.js";
-
-interface templateOptions extends Options {
-  doctype: "html" | string;
-}
-
-interface pugTemplateObject {
-  content: string;
-}
-
-type pugTemplate = string | pugTemplateObject;
-
-function convertToValidPugInsertion(htmlSyntaxToConvert: string): string {
-  const trimedHTMLSyntaxToConvert = htmlSyntaxToConvert.trim();
-  if (!trimedHTMLSyntaxToConvert.match(/^<.+>$/s)) return "";
-  return (trimedHTMLSyntaxToConvert.slice(
-    1,
-    trimedHTMLSyntaxToConvert.length - 1,
-  ));
-}
+type template = string
 
 async function createFileFromTemplate(
   pathToCreateTheFile: string,
@@ -41,25 +20,19 @@ async function createFileFromTemplate(
 }
 
 async function templateCompiler(
-  pugTemplate: pugTemplate,
-  localContent?: LocalsObject,
+  template: template,
   options?: Options,
 ): Promise<string> {
-  const templateOptions: templateOptions = {
-    doctype: options?.doctype ?? "html",
+  const templateOptions: Options = {
     ...options,
+    async: true,
+    cache: true,
+    rmWhitespace: true,
   };
 
-  let compilerPugFunction;
-
-  if (typeof pugTemplate === "string") {
-    const pugFile = await Deno.readFile(resolve(__root_dirname, pugTemplate));
-    compilerPugFunction = pug.compile((new TextDecoder()).decode(pugFile), templateOptions);
-  } else {
-    compilerPugFunction = pug.compile(pugTemplate.content, templateOptions);;
-  }
-
-  return compilerPugFunction({ ...localContent });
+  const data = await ejs.renderFile(resolve(__root_dirname, template), {}, templateOptions);
+  
+  return data;
 }
 
 if (argvFlags.help) {
@@ -70,14 +43,11 @@ if (argvFlags.help) {
   argvFlags._.length >= 2 &&
   (typeof argvFlags._[0] == "string" && typeof argvFlags._[1] == "string")
 ) {
-  const pugTemplate = await templateCompiler(argvFlags._[0], {
-    headTags: convertToValidPugInsertion(htmlSyntax),
-  }, { pretty: "\t" });
-  createFileFromTemplate(argvFlags._[1], pugTemplate);
+  const template = await templateCompiler(argvFlags._[0]);
+  createFileFromTemplate(argvFlags._[1], template);
 }
 
-templateCompiler.convertToValidPugInsertion = convertToValidPugInsertion;
 templateCompiler.createFileFromTemplate = createFileFromTemplate;
 
 export default templateCompiler;
-export { convertToValidPugInsertion, createFileFromTemplate };
+export { createFileFromTemplate };
