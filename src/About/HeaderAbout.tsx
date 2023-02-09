b47c57f452c124023322ce5cd9141f6cf5880731
fantonix <fantonix.web@gmail.com>
2023-02-08 20:09:42 -0400

    fix(typescript): Transforming React.RefObject to React.ForwardedRef in HeaderAbout

--- src/About/HeaderAbout.tsx
+++ src/About/HeaderAbout.tsx
@@ -2,7 +2,7 @@ import React from "https://esm.sh/react@18.2.0";
 import ContainerImgMe from "./ContainerImgMe.tsx";
 
 const HeaderAbout = React.forwardRef(
-  function HeaderAbout(_props, ref: React.RefObject<HTMLDivElement>) {
+  function HeaderAbout(_props, ref: React.ForwardedRef<HTMLDivElement>) {
     return (
       <div className="flexing cajita" ref={ref}>
         <ContainerImgMe nameContainer="simon-antonio" />
