780b3c70920c6fa3638e9fca32e354d9301154df
fantonix <fantonix.web@gmail.com>
2023-02-13 11:12:28 -0400

    perf|feat(turborepo): Adding turborepo, removing dist cached files from git and more
    Adding the turborepo tool to manage the build process and make it faster
    Updating the scripts in package.json and the tasks in deno.json
    Using turbo-build from the main.tsx file to build the dist folder but
    the astro folder, that is not compatible with deno runtime
    Creating srcBuilder.ts from the srcBuilder.bash file and
    Creating a sassBuilder.ts file that creates the sass file and
    export it to the output file, there are a lot to modify here, but it
    is a nice feature now

---
+++ srcBuilder.ts
@@ -0,0 +1,127 @@
+import { sleep } from "https://deno.land/x/sleep@v1.2.1/mod.ts";
+
+const toDeno = [
+  "src/About/About.tsx",
+  "src/Errors/NotFound.tsx",
+  "src/MainPage/MainPage.tsx",
+  "src/Work/Work.tsx",
+  "src/Works/Works.tsx",
+];
+const fromDeno = [
+  "dist/DenoAbout.js",
+  "dist/DenoNotFound.js",
+  "dist/DenoMainPage.js",
+  "dist/DenoWork.js",
+  "dist/DenoWorks.js",
+];
+const fromEsbuild = [
+  "dist/About.js",
+  "dist/NotFound.js",
+  "dist/MainPage.js",
+  "dist/WorkWrapper.js",
+  "dist/Works.js",
+];
+
+//  Deno Bundle functions
+async function _createDenoBundle(from: string, to: string) {
+    const denoBuildProcess = Deno.run({
+        cmd: ["deno", "bundle", "--no-check", from, to],
+        cwd: Deno.cwd(),
+    })
+    
+    return (await denoBuildProcess.status()).success;
+}
+
+async function createDenoBundle() {
+    let count = 0;
+    const maxTimeout = 15;
+    let timeout = 0;
+
+    for (const filePath in toDeno) {
+        _createDenoBundle(toDeno[filePath], fromDeno[filePath]).then(success => {
+            if (!success) {
+                console.error("There was an error! Something wasn't bundled correctly");
+            } else {
+                count++;
+            }
+        });
+    }
+
+    while (count !== toDeno.length) {
+        if (timeout == maxTimeout) return;
+        await sleep(1);
+        timeout++;
+    }
+    return
+}
+
+// Esbuild Bundle functions
+async function _createEsbuildBundle(from: string, to: string) {
+    const outfile="--outfile=" + to;
+
+    const esbuildBuildProcess = Deno.run({
+        cmd: ["deno", "run", "-A", "npm:esbuild", "--bundle", from, "--minify", outfile],
+        cwd: Deno.cwd(),
+    });
+
+    return (await esbuildBuildProcess.status()).success;
+}
+
+function createEsbuildBundle(from: string, to: string) {
+    _createEsbuildBundle(from, to).then(success => {
+        if (!success) {
+            console.error("There was an error! Something wasn't bundled correctly")
+        } else {
+            Deno.removeSync(from);
+        }
+    });
+}
+
+// Sass compiler function
+
+async function compileSass() {
+    const sassBuildProcess = Deno.run({
+        cmd: ["deno", "run", "-A", "sassBuilder.ts", "styles/index.scss", "dist/index.css"],
+        cwd: Deno.cwd(),
+    });
+
+    await sassBuildProcess.status();
+}
+
+// Astro compiler function
+
+function compileAstro() {
+    Deno.run({
+        cmd: ["deno", "run", "-A", "npm:astro", "build"],
+        cwd: Deno.cwd(),
+    })
+}
+
+/* I made this from the srcBuilder.sh function, the problem to solve is that it works with node_modules */
+// Bundle creator function
+// It will create the bundles from Deno first
+// and then those bundles with will bundled again with esbuild
+// It's pretty fast everything
+async function createBundles() {
+    const installEsbuildProcess = Deno.run({
+        cmd: ["deno", "install", "npm:esbuild"]
+    });
+    await installEsbuildProcess.status();
+
+    await createDenoBundle();
+    // createEsbuildBundle depends on createDenoBundle
+    // We have to wait until the deno bundle is created
+    // To create the esbuild bundle
+    for (const filePath in fromDeno) {
+        createEsbuildBundle(fromDeno[filePath], fromEsbuild[filePath]);
+    }
+
+    const installSassProcess = Deno.run({
+        cmd: ["deno", "install", "npm:sass-embedded"]
+    });
+    await installSassProcess.status();
+    compileSass();
+    // compileAstro();
+}
+
+createBundles(); 
