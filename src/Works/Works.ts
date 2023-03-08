696b127a37b1e6fc82630bddc0c8dd8b111df05a
fantonix <fantonix.web@gmail.com>
2023-03-07 21:48:32 -0400

    refactor(src/Works): Transforming Works to Vue

---
+++ src/Works/Works.ts
@@ -0,0 +1,13 @@
+export default {
+    data() {
+        return {
+            proto: "type"
+        }
+    },
+    template: `
+    <div>
+        {{ proto }}
+    </div>
+    `,
+    name: "Works",
+}
--- src/Works/index.ts
+++ src/Works/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./Works.tsx";
+export { default } from "./Works.ts";
