f3640fccb53823d1164ee485c2a1bfb57d62683a
fantonix <fantonix.web@gmail.com>
2023-05-09 15:56:40 -0400

    refactor/feat(src/components):
    Creating the MenuButton component and refactoring Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,60 +1,24 @@
-import {
-  briefcaseSvg,
-  houseSvg,
-  pencilSvg,
-  pencilWorkspaceSvg,
-  xSquareSvg,
-} from "./Utils/Svg.tsx";
+import { useStore } from "@nanostores/react";
+import { isMenuOpen } from "../stores/menuStore.ts";
 
-interface Menu {
-  changeVisibilityMenu: () => void;
-}
+const menuItems = ["Home", "About", "Works", "Contact"] as const;
 
-export default function Menu({ changeVisibilityMenu }: Menu) {
-  return (
-    <div className="menu" id="menu" hidden={true}>
-      <div className="menu-base-body">
-        <div className="menu-button-div">
-          <button onClick={() => changeVisibilityMenu()}>
-            {xSquareSvg}
-          </button>
-        </div>
-        <div className="menu-items-parent">
-          <div className="menu-item">
-            <a href="#home" onClick={() => changeVisibilityMenu()}>
-              <div>
-                <img src="/static/media/images/home.jpg" alt="" />
-              </div>
+export default function Menu() {
+  let $isMenuOpen = useStore(isMenuOpen);
+
+  return $isMenuOpen ? (
+    <div id="menu" class="animate__animated animate__fadeInLeft">
+      <div id="menu-base">
+        {menuItems.map((value) => {
+          return <div class="menu-item">
+            <a class="menu-item-box" href={"#" + value.toLowerCase()} onClick={(e) => isMenuOpen.set(!$isMenuOpen)}>
+              <h3>{value}</h3>
             </a>
-            <h3>{houseSvg} Home</h3>
           </div>
-          <div className="menu-item">
-            <a href="/about" onClick={() => changeVisibilityMenu()}>
-              <div>
-                <img src="/static/media/images/about.jpg" alt="" />
-              </div>
-            </a>
-            <h3>{pencilWorkspaceSvg} About</h3>
-          </div>
-          <div className="menu-item">
-            <a href="/works" onClick={() => changeVisibilityMenu()}>
-              <div>
-                <img src="/static/media/images/works.jpg" alt="" />
-              </div>
-            </a>
-            <h3>{pencilSvg} Works</h3>
-          </div>
-          <div className="menu-item">
-            <a href="#Contact" onClick={() => changeVisibilityMenu()}>
-              <div>
-                <img src="/static/media/images/contact.jpg" alt="" />
-              </div>
-            </a>
-            <h3>{briefcaseSvg} Contact</h3>
-          </div>
-        </div>
+        })}
       </div>
-      <div className="menu-base-footer" />
     </div>
+  ) : (
+    <></>
   );
 }
